8c138658c6b01521bf870b33b4033c573ffff62d
Dave Blaschke <63350055+deblaschke@users.noreply.github.com>
2020-10-25 11:38:39 -0500

    Support film camera slides

--- js/description_yellowstone.js
+++ js/description_yellowstone.js
@@ -11,51 +11,57 @@ function getDescription(path) {
   if (index >= 0 && path.lastIndexOf('.jpg') > index) {
     var file = path.substring(index + 1, path.lastIndexOf('.'));
 
-    // file begins with "nnn_" for slides
-    if (file.match(/^[0-9]{3}_/) && file.length >= 12) {
-      // Picture name is first 8 characters after "nnn_"
-      result = file.substring(4, 12);
-
-      // Special handling for cellular phone camera picture names
-      if (file.match(/^[0-9]{3}_[0-9]{8}T[0-9]{6}/)) {
-        result = result + file.substring(13, 19);
+    // File begins with "nnn_" for slides
+    if (file.match(/^[0-9]{3}_/)) {
+      if (file.match(/^[0-9]{3}_[C-Z]{1}[0-9]{7}/)) {
+        // Found digital camera picture name ("nnn_Annnnnnn")
+        result = file.substring(4, 12);
+      } else if (file.match(/^[0-9]{3}_[A-B]{1}[0-9]{3}-[0-9]{2}/)) {
+        // Found film camera picture name ("nnn_Annn-nn")
+        result = file.substring(4, 11);
+      } else if (file.match(/^[0-9]{3}_[0-9]{8}T[0-9]{6}/)) {
+        // Found cell phone camera picture name ("nnn_nnnnnnnnTnnnnnn")
+        result = file.substring(4, 12) + file.substring(13, 19);
       }
 
-      // Handle description if present
-      index = file.indexOf('-');
-      if (index >= 0) {
-        // Picture description is everything after "-"
-        var desc = file.substring(index + 1);
-
-        // Remove "*Yellowstone National Park" from description
-        index = desc.indexOf("_in_Yellowstone_National_Park");
+      // Continue processing if recognized picture name
+      if (result.length > 0) {
+        // Handle description if present
+        index = file.indexOf('-', result.length + 4);
         if (index >= 0) {
-          desc = desc.substring(0, index);
-        } else {
-          index = desc.indexOf("Yellowstone_National_Park");
+          // Picture description is everything after "-"
+          var desc = file.substring(index + 1);
+
+          // Remove "*Yellowstone National Park" from description
+          index = desc.indexOf("_in_Yellowstone_National_Park");
           if (index >= 0) {
             desc = desc.substring(0, index);
-          }
-        }
-
-        // Process description if one exists
-        if (desc.length > 0) {
-          // Replace underscores with spaces
-          result = result + " - " + desc.replace(/_/g, ' ');
-
-          // Replace special characters ("[*]") with HTML entity names ("&*;")
-          index = file.indexOf('[');
-          if (index >= 0 && index < file.indexOf(']')) {
-            result = result.replace(/\[/g, '&');
-            result = result.replace(/\]/g, ';');
-          }
-
-          // Break up description over 80 characters in length into two lines
-          if (result.length > 80) {
-            index = result.lastIndexOf(" ", 80);
+          } else {
+            index = desc.indexOf("Yellowstone_National_Park");
             if (index >= 0) {
-              var newResult = result.substring(0, index) + "<BR>" + result.substring(index+1);
-              result = newResult;
+              desc = desc.substring(0, index);
+            }
+          }
+
+          // Process description if one exists
+          if (desc.length > 0) {
+            // Replace underscores with spaces
+            result = result + " - " + desc.replace(/_/g, ' ');
+
+            // Replace special characters ("[*]") with HTML entity names ("&*;")
+            index = file.indexOf('[');
+            if (index >= 0 && index < file.indexOf(']')) {
+              result = result.replace(/\[/g, '&');
+              result = result.replace(/\]/g, ';');
+            }
+
+            // Break up description over 80 characters in length into two lines
+            if (result.length > 80) {
+              index = result.lastIndexOf(" ", 80);
+              if (index >= 0) {
+                var newResult = result.substring(0, index) + "<BR>" + result.substring(index+1);
+                result = newResult;
+              }
             }
           }
         }
@@ -63,7 +69,7 @@ function getDescription(path) {
     }
 
     // Append trip year if valid dir format
-    var dir = path.match(/\/20[0-9]{2}(-[0-9]{2})?[A-Z]*\//gi);
+    var dir = path.match(/\/[0-9]{4}(-[0-9]{2})?[A-Z]*\//gi);
     if (dir != null) {
       if (dir[0].lastIndexOf('-') >= 5 && dir[0].length >= 8) {
         result = result + " (" + dir[0].substring(1,8) + ")";
